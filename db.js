b6dea64e296efb68220fe1b18ee7ec42e98ae375
Ayush Raj <ayushcode1@gmail.com>
2023-04-17 01:34:03 +0530

    mongodb connection

--- db.js
+++ db.js
@@ -1,24 +1,3 @@
-const express = require('express');
-
-const app = express();
-const PORT = 3000;
-
-app.use(express.json());
-app.post("/h", (req, res) => {
-  const { name } = req.body;
-
-  res.send(`Welcome ${name}`);
-});
-
-app.get("/", (req, res) => {
-  res.status(200);
-  res.send("Welcome to root URL of Server");
-});
-
-app.get("/hello", (req, res) => {
-  res.set("Content-Type", "text/html");
-  res.status(200).send("<h1>Hello GFG Learner!</h1>");
-});
 const { MongoClient } = require("mongodb");
 
 // Replace the uri string with your connection string.
@@ -43,11 +22,3 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-app.listen(PORT, (error) =>{
-	if(!error)
-		console.log("Server is Successfully Running,and App is listening on port "+ PORT)
-    else
-		console.log("Error occurred, server can't start", error);
-	}
-);
